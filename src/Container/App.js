8ab6d51fb12af7470e5c21344296f6e64e2a8212
frfn <flexer.fadrigalan@gmail.com>
2020-05-11 15:34:10 -0400

    boiler plate for BurgerBuilder

---
+++ src/Container/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Container/App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
