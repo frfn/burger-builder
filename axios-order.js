374d5af7b64dd6333cf8ec99fbdaa0d173c34890
frfn <flexer.fadrigalan@gmail.com>
2020-06-13 17:20:26 -0400

    creating axios instance for baseURL

---
+++ axios-order.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+/* variable created so I can EXPORT VARIABLE */
+const instance = axios.create({
+    baseURL: 'https://react-my-burger-b4a98.firebaseio.com/'
+})
+
+export default instance;
