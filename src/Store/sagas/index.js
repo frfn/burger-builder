8e1dc263ef03e3a6042e9c80272174ca66113d6e
frfn <flexer.fadrigalan@gmail.com>
2020-11-23 12:00:03 -0500

    repush

--- src/Store/sagas/index.js
+++ src/Store/sagas/index.js
@@ -2,6 +2,7 @@ import { takeEvery } from "redux-saga/effects";
 import { logoutSaga } from "./auth";
 import * as actionTypes from "../actions/actionTypes";
 
+/* listener */
 export function* watchAuth() {
 	yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
 }
